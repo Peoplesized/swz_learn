a583d4bf9ff1fcb56c428678a44930adfea35faa
superuser-chi <mavusogiftntokozo@gmail.com>
2019-12-12 10:22:51 +0200

    add get all keys logic

--- 2019-12-05-json-describe-tool/describe.js
+++ 2019-12-05-json-describe-tool/describe.js
@@ -1,7 +1,51 @@
 function describe(json, level = 1) {
+  console.log(`--keys on all levels`)
+  getKeys(json).forEach( k => {
+    console.log(` -${k}`)
+  })
+}
+function getKeys(obj) {
+  var all = {}; 
+  var seen = [];
+  checkValue(obj, all, seen);
+  return Object.keys(all);
+}
+
+function checkValue(value, all, seen) {
+  if (Array.isArray(value)) return checkArray(value, all, seen);
+  if (value instanceof Object) return checkObject(value, all, seen);
+}
+function checkArray(array, all, seen) {
+  if (seen.indexOf(array) >= 0) return;
+  seen.push(array);
+  for (var i = 0, l = array.length; i < l; i++) {
+    checkValue(array[i], all, seen);
+  }
+}
+function checkObject(obj, all, seen) {
+  if (seen.indexOf(obj) >= 0) return;
+  seen.push(obj);
+  var keys = Object.keys(obj);
+  for (var i = 0, l = keys.length; i < l; i++) {
+    var key = keys[i];
+    all[key] = true;
+    checkValue(obj[key], all, seen);
+  }
+}
 
 
-console.log(json, parseInt(level));
+function iterate(obj, stack) {
+  test.push(Object.keys(obj))
+  for (var property in obj) {
+      if (obj.hasOwnProperty(property)) {
+          if (typeof obj[property] == "object") {
+              iterate(obj[property], stack + '.' + property);
+          }
+           else {
+              console.log(property)
+          }
+      }
+  }
 }
 
 module.exports = {
