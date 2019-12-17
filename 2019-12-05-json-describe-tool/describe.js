219785fc5f0e2012abadc3de4abb475e92adfe32
superuser-chi <mavusogiftntokozo@gmail.com>
2019-12-17 09:08:14 +0200

    simplify get keys function

--- 2019-12-05-json-describe-tool/describe.js
+++ 2019-12-05-json-describe-tool/describe.js
@@ -1,52 +1,28 @@
+count = 0;
+g = {};
+function getKeys(obj, parent=NaN) {
+  var keys = [];
+  let temp = count
+  if(typeof obj !== "object"){
+    return typeof obj
+  }else if (!Array.isArray(obj)&& obj instanceof Object) {
+    count++;
+    keys = Object.keys(obj);
+    keys.forEach(k => {
+      g[k] ={ level: temp, keys: getKeys(obj[k],k), parent: parent};
+    });
+    return keys
+  } else if (Array.isArray(obj)){
+    return obj.length
+  }
+}
 function describe(json, level = 1) {
   console.log(`--keys on all levels`)
   getKeys(json).forEach( k => {
     console.log(` -${k}`)
   })
 }
-function getKeys(obj) {
-  var all = {}; 
-  var seen = [];
-  checkValue(obj, all, seen);
-  return Object.keys(all);
-}
 
-function checkValue(value, all, seen) {
-  if (Array.isArray(value)) return checkArray(value, all, seen);
-  if (value instanceof Object) return checkObject(value, all, seen);
-}
-function checkArray(array, all, seen) {
-  if (seen.indexOf(array) >= 0) return;
-  seen.push(array);
-  for (var i = 0, l = array.length; i < l; i++) {
-    checkValue(array[i], all, seen);
-  }
-}
-function checkObject(obj, all, seen) {
-  if (seen.indexOf(obj) >= 0) return;
-  seen.push(obj);
-  var keys = Object.keys(obj);
-  for (var i = 0, l = keys.length; i < l; i++) {
-    var key = keys[i];
-    all[key] = true;
-    checkValue(obj[key], all, seen);
-  }
-}
-
-
-function iterate(obj, stack) {
-  test.push(Object.keys(obj))
-  for (var property in obj) {
-      if (obj.hasOwnProperty(property)) {
-          if (typeof obj[property] == "object") {
-              iterate(obj[property], stack + '.' + property);
-          }
-           else {
-              console.log(property)
-          }
-      }
-  }
-}
 
 module.exports = {
   describe,
--- 2019-12-05-json-describe-tool/test.js
+++ 2019-12-05-json-describe-tool/test.js
@@ -6,6 +6,11 @@ obj = {
       objects_all_the_way: 'down',
       'yet another': {
         test: 'continuation',
+        "help": {
+          "what": {
+            "level": "is this on"
+          }
+        },
       },
     },
     testing: 'describe tool',
@@ -30,53 +35,66 @@ obj = {
 //return all keys in an object
 count = 0;
 g = {};
-function getKeys(obj) {
+function getKeys(obj, parent=NaN) {
   var keys = [];
   let temp = count
-  if (obj instanceof Object && !Array.isArray(obj)) {
+  if(typeof obj !== "object"){
+    return typeof obj
+  }else if (!Array.isArray(obj)&& obj instanceof Object) {
+    count++;
     keys = Object.keys(obj);
     keys.forEach(k => {
-      g[k] ={level: temp, keys: getKeys(obj[k])};
+      g[k] ={ level: temp, keys: getKeys(obj[k],k), parent: parent};
     });
-  } else if (Array.isArray(obj)) {
-    keys = Object.keys(obj[0]);
+    return keys
+  } else if (Array.isArray(obj)){
+    return obj.length
   }
-  count++;
-  if(typeof obj === "string"){
-    keys = typeof obj
-  }
-  return keys;
 }
 
-Array.prototype.remove = function() {
-  var what, a = arguments, L = a.length, ax;
-  while (L && this.length) {
-      what = a[--L];
-      while ((ax = this.indexOf(what)) !== -1) {
-          this.splice(ax, 1);
-      }
+function find(jsonObj, param) {
+  if( jsonObj !== null && typeof jsonObj == "object" ) {
+      Object.entries(jsonObj).forEach(([key, value]) => {
+          // key is either an array index or object key
+          if(key === param ){
+            return true
+          } else {
+            traverse(value);
+          }
+      });
   }
-  return this;
-};
+  return false
+}
+
+// Array.prototype.remove = function() {
+//   var what, a = arguments, L = a.length, ax;
+//   while (L && this.length) {
+//       what = a[--L];
+//       while ((ax = this.indexOf(what)) !== -1) {
+//           this.splice(ax, 1);
+//       }
+//   }
+//   return this;
+// };
 function getKeysOnLevel(obj, level = 0) {
   getKeys(obj);
   temp = Object.keys(g);
   console.log(`--level ${level}`)
   for(let i=0; i <= level; i++ ){
     temp.forEach(k => {
-      if (g[k].level === i) {
+      if (g[k].level <= i) {
         console.log(`${" ".repeat(i)}${k}`);
         if((g[k].level + 1 <= level) && Array.isArray(g[k].keys)){
           g[k].keys.forEach(e => {
             console.log(`${" ".repeat(i + 1)}-${e}`)
             delete g[k][e]
-            
           })
         }
       }
     });    
   }
 }
-getKeysOnLevel(obj, 2);
-console.log(g)
+
+getKeysOnLevel(obj, 1)
+console.log(g);
 
